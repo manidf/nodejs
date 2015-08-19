fc72dd718585b08d0e4377a0863388d6f218c3cd
Mannuel Ferreira <mannuel@wwc.co.za>
2015-08-19 05:56:25 +0200

    added hogan-express and piped to index page login page

--- ChatCat_Begin/app.js
+++ ChatCat_Begin/app.js
@@ -1,11 +1,24 @@
 
 // require express frame work
 var express = require('express'),
-    app = express();
+    app = express(),
+    path = require('path');
+
+// set hogan view paths to point to views directory
+app.set('views', path.join(__dirname, 'views'));
+
+// hogan templating engine
+app.engine('html', require('hogan-express'));
+app.set('view engine', 'html');
+
+// set express static assets lets express find all static files in ./public
+app.use(express.static(path.join(__dirname, 'public')));
 
 // define route
 app.route('/').get(function(req, res, next){
-   res.send( '<h1>Hello World!</h1>' );
+   //res.send( '<h1>Hello World!</h1>' );
+    // respones.render, render index file inside the views folder
+    res.render('index', {})
 });
 
 // define port number and log to console using callback function
